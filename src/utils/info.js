9775d81276df6a46531df02dc4539d5f30658aed
Evil-Developer <pro.expert0525@gmail.com>
2023-01-24 18:44:43 +0200

    New: info.js

---
+++ src/utils/info.js
@@ -0,0 +1,122 @@
+export const verticalLines = [
+  {
+    position: {
+      x: 0.5,
+      y: 0,
+      z: 0,
+    },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    radius: 0.04,
+    length: 18,
+    color: '#b3b3b3',
+  },
+  {
+    position: {
+      x: 1.5,
+      y: 0,
+      z: 0,
+    },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    radius: 0.04,
+    length: 18,
+    color: '#b3b3b3',
+  },
+  {
+    position: {
+      x: 2.5,
+      y: 0,
+      z: 0,
+    },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    radius: 0.04,
+    length: 18,
+    color: '#b3b3b3',
+  },
+  {
+    position: {
+      x: 2.55,
+      y: 0,
+      z: 0,
+    },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    radius: 0.06,
+    length: 18,
+    color: '#b36b00',
+  },
+  {
+    position: {
+      x: -0.5,
+      y: 0,
+      z: 0,
+    },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    radius: 0.04,
+    length: 18,
+    color: '#b3b3b3',
+  },
+  {
+    position: {
+      x: -1.5,
+      y: 0,
+      z: 0,
+    },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    radius: 0.04,
+    length: 18,
+    color: '#b3b3b3',
+  },
+  {
+    position: {
+      x: -2.5,
+      y: 0,
+      z: 0,
+    },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    radius: 0.04,
+    length: 18,
+    color: '#b3b3b3',
+  },
+  {
+    position: {
+      x: -2.55,
+      y: 0,
+      z: 0,
+    },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    radius: 0.06,
+    length: 18,
+    color: '#b36b00',
+  },
+];
